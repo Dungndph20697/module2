c60c85074b3557521eca436443ac54353b1a6324
Dungndph20697 <dungthoai321@gmail.com>
2025-07-04 13:48:11 +0700

    th tính số ngày trong tháng

---
+++ th_tinh_so_ngay_trong_thang/src/main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tháng");
+        int mont = sc.nextInt();
+        String daysInMonth;
+        switch (mont) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
+            case 2:
+                daysInMonth = "29 hoặc 30";
+            default:
+                daysInMonth = "";
+        }
+        if (daysInMonth.equals("")) {
+            System.out.println("Tháng không chính xác");
+        } else {
+            System.out.println("Tháng " + mont + " có " + daysInMonth + " ngày");
+        }
+    }
+}
