307afd9a9f01ab148b71a2b6c30980ae756ec02c
Dungndph20697 <dungthoai321@gmail.com>
2025-07-10 11:22:13 +0700

    th static method

---
+++ bt_th_module2/src/baitap/xaydunglopstopwatch/StopWatch.java
@@ -0,0 +1,32 @@
+package baitap.xaydunglopstopwatch;
+
+public class StopWatch {
+    private long startTime;
+    private  long endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public  void start(){
+
+    }
+
+
+}
---
+++ bt_th_module2/src/baithuchanh/thstaticmethod/Student.java
@@ -0,0 +1,44 @@
+package baithuchanh.thstaticmethod;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    public Student(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static String getCollege() {
+        return college;
+    }
+
+    public static void setCollege(String college) {
+        Student.college = college;
+    }
+
+    static void change() {
+        college = "CODEGYM";
+    }
+
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
