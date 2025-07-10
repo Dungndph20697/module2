95e41f6a9686a3e0d8c8a772b24e21977ada0153
Dungndph20697 <dungthoai321@gmail.com>
2025-07-10 11:44:44 +0700

    bt access modifier

---
+++ bt_th_module2/src/baitap/accessmodifier/TestCircle.java
@@ -0,0 +1,11 @@
+package baitap.accessmodifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c = new Circle(2);
+
+        System.out.println("Radius: "+c.getRadius());
+        System.out.println("Area: "+c.getArea());
+
+    }
+}
