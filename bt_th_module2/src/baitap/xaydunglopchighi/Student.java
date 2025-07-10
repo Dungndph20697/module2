f6d0db1caf60b9e58bb5f02b896770b5912d5070
Dungndph20697 <dungthoai321@gmail.com>
2025-07-10 11:50:23 +0700

    bt xây dựng lớp chỉ ghi

---
+++ bt_th_module2/src/baitap/xaydunglopchighi/Student.java
@@ -0,0 +1,18 @@
+package baitap.xaydunglopchighi;
+
+public class Student {
+    public String name = "John";
+    public String classes = "CO2";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+}
---
+++ bt_th_module2/src/baitap/xaydunglopchighi/Test.java
@@ -0,0 +1,9 @@
+package baitap.xaydunglopchighi;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Dung");
+        s1.setClasses("C05");
+    }
+}
