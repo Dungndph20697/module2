d0de3c8adb8c0ed9ef5b49fc6032341ccb7ba16c
Dungndph20697 <dungthoai321@gmail.com>
2025-07-14 14:15:16 +0700

    th triển khai interface Comparable cho các lớp hình học

--- bt_th_module2/src/baithuchanh/hecacdoituonghinhhoc/Circle.java
+++ bt_th_module2/src/baithuchanh/hecacdoituonghinhhoc/Circle.java
@@ -10,6 +10,19 @@ public class Circle extends Shape {
     public Circle() {
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     public double getArea() {
         return Math.PI * radius * radius;
     }
