55d379b718a32b78bce63fb48772c552f2416557
Dungndph20697 <dungthoai321@gmail.com>
2025-07-23 16:19:50 +0700

    sua bt sap xep chen

--- bt_th_module2/src/baitap/sapxepcchen/InsertionSort.java
+++ bt_th_module2/src/baitap/sapxepcchen/InsertionSort.java
@@ -8,10 +8,9 @@ public class InsertionSort {
         int n = arr.length;
 
         for (int i = 1; i < n; i++) {
-            int key = arr[i]; // phần tử cần chèn
+            int key = arr[i]; 
             int j = i - 1;
 
-            // Di chuyển các phần tử lớn hơn key về phía sau
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j--;
